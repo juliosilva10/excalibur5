45ef6acfdadbfaa60c185c14e9a6a6511d94f701
juliosilva10 <jota_c84@hotmail.com>
2026-05-15 21:10:01 -0300

    feat: extract MartingaleRecoverStrategy from StrategyExecutor

---
+++ Services/Strategy/Recovery/MartingaleRecoverStrategy.cs
@@ -0,0 +1,37 @@
+namespace Excalibur5.Services.Strategy.Recovery;
+
+public sealed class MartingaleRecoverStrategy : IRecoverStrategy
+{
+    private readonly decimal _factor;
+    private readonly int _maxLevel;
+    private int _level;
+
+    public MartingaleRecoverStrategy(decimal factor, int maxLevel)
+    {
+        _factor = factor;
+        _maxLevel = maxLevel;
+    }
+
+    public decimal GetNextStake(RecoverContext context)
+    {
+        if (_level <= 0)
+            return context.BaseStake;
+
+        var stake = context.BaseStake;
+        for (int i = 0; i < _level; i++)
+            stake *= _factor;
+        return Math.Round(stake, 2);
+    }
+
+    public void RecordResult(decimal profit, decimal stakeUsed)
+    {
+        if (profit >= 0)
+            _level = 0;
+        else if (_level < _maxLevel)
+            _level++;
+        else
+            _level = 0;
+    }
+
+    public void Reset() => _level = 0;
+}
